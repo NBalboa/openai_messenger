4b8383267d74c530a57e9689854b31fa9812105c
Nicko Balboa <76141006+NBalboa@users.noreply.github.com>
2024-02-05 03:30:51 +0800

    open ai messenger

---
+++ helper/openaiApi.js
@@ -0,0 +1,38 @@
+const { OpenAI } = require("openai");
+require("dotenv").config();
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const chatCompletion = async (prompt) => {
+    try {
+        const response = await openai.chat.completions.create({
+            messages: [
+                {
+                    role: "system",
+                    content: "You are a helpful assistant.",
+                },
+                { role: "user", content: prompt },
+            ],
+            model: "gpt-3.5-turbo",
+            temperature: 0.8,
+        });
+
+        let content = response.choices[0].message.content;
+
+        return {
+            status: 1,
+            response: content,
+        };
+    } catch (error) {
+        return {
+            status: 0,
+            response: "Please check openai API key.",
+        };
+    }
+};
+
+module.exports = {
+    chatCompletion,
+};
