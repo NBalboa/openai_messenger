eba168ee5c0f255d41796f0893bc31081710e093
Nicko Balboa <76141006+NBalboa@users.noreply.github.com>
2024-02-10 05:46:44 +0800

    added a general config to gemini

--- helper/geminiApi.js
+++ helper/geminiApi.js
@@ -1,11 +1,21 @@
 require("dotenv").config();
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const generationConfig = {
+    stopSequences: ["red"],
+    maxOutputTokens: 250,
+    temperature: 0.9,
+    topP: 0.1,
+    topK: 16,
+};
 
 const genAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 async function chatGemini(prompt) {
     try {
-        const model = genAi.getGenerativeModel({ model: "gemini-pro" });
+        const model = genAi.getGenerativeModel({
+            model: "gemini-pro",
+            generationConfig,
+        });
         const result = await model.generateContent(prompt);
         const response = await result.response;
         const text = response.text();
