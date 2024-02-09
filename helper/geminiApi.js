bda4f18cfc7764809e2a0257e3f5af58226f9ef6
Nicko Balboa <76141006+NBalboa@users.noreply.github.com>
2024-02-10 05:18:53 +0800

    chat gemini display text

--- helper/geminiApi.js
+++ helper/geminiApi.js
@@ -9,6 +9,7 @@ async function chatGemini(prompt) {
         const result = await model.generateContent(prompt);
         const response = await result.response;
         const text = response.text();
+        console.log(text);
 
         return {
             status: 1,
